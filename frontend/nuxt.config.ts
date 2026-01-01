8cdb7cf24e3f22bedbb7dfb8227b6f90b41f43a8
Arnaud Steiner <arnaudsteiner@orange.fr>
2026-01-02 00:50:36 +0100

    fix: prod env variables

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,10 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	nitro: {
-		host: process.env.HOST || '0.0.0.0',
-		port: process.env.PORT || 3000
-	},
-
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: process.env.NODE_ENV === 'development' },
 	future: {
